2d823d9140744884d72ab8891ac0800f5ddf5a47
jelena2803 <jelenaiva2803@gmail.com>
2024-01-25 23:32:34 +0200

    rendering is commented out, still has to fixed and tested

--- frontend/src/components/Residences.jsx
+++ frontend/src/components/Residences.jsx
@@ -2,24 +2,31 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import ResidenceCard from "./ResidenceCard";
 import "../css/Residences.css";
+// import { useNavigate } from "react-router-dom";
 
 const Residences = () => {
   const [residences, setResidences] = useState([]);
-
+  
   useEffect(() => {
     fetchData()
   }, []);
 
 console.log(residences);
 
+// const navigate = useNavigate();
+
 const fetchData = async () => {
-  const res = await axios.get("http://localhost:3001/residences", {withCredentials:true})
- setResidences(res.data)
+  axios
+  .get("http://localhost:3001/residences", {withCredentials:true})
+  .then(({data}) => {
+    console.log(data)
+  })
+//  setResidences(res.data)
 }
 
   return (
     <div className="exercise-page">
-      <div className="exercise-list">
+      {/* <div className="exercise-list">
         {residences.map((residence) => {
           return (
             <ResidenceCard
@@ -28,7 +35,9 @@ const fetchData = async () => {
             ></ResidenceCard>
           );
         })}
-      </div>
+      </div> */}
+
+      <p>Test</p>
     </div>
   );
 };
