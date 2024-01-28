db66b300235739c93f4d9d3bb07bcc67a13036bb
jelena2803 <jelenaiva2803@gmail.com>
2024-01-28 02:53:13 +0200

    delete confirmation modal element and delete functionality added

--- frontend/src/components/Residences.jsx
+++ frontend/src/components/Residences.jsx
@@ -1,10 +1,24 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import ResidenceCard from "./ResidenceCard";
+import {
+  MDBBtn,
+  MDBModal,
+  MDBModalDialog,
+  MDBModalContent,
+  MDBModalHeader,
+  MDBModalTitle,
+  MDBModalBody,
+  MDBModalFooter,
+} from 'mdb-react-ui-kit';
 import "../css/Residences.css";
 
 const Residences = () => {
   const [residences, setResidences] = useState([]);
+  const [show, setShow] = useState(false);
+  const [residenceId, setResidenceId] = useState("");
+  const handleClose = () => setShow(false);
+  const showAlert = () => setShow(true);
 
   useEffect(() => {
     fetchData()
@@ -18,23 +32,49 @@ const Residences = () => {
       })
   }
 
+  // show delete confirmation box
+  const handleDeleteAlert = (id) => {
+    setResidenceId(id);
+    showAlert();
+    setTimeout(() => {
+      setShow(false);
+    }, 2000);
+  };
+
+  // handle delete residence
   const deleteResidence = async (id) => {
     await axios.delete(`http://localhost:3001/residences/${id}`, {
       withCredentials: true,
     });
-    fetchData()
+    fetchData();
+    handleClose();
+    handleDeleteAlert();
   };
 
   return (
     <div className="exercise-page">
       <div className="exercise-list">
-        {residences.map((residence) => {
+        {show && (
+          <MDBModal open={show} tabIndex='-1'>
+            <MDBModalDialog>
+              <MDBModalContent>
+                <MDBModalHeader>
+                  <MDBModalTitle>Delete confirmation</MDBModalTitle>
+                </MDBModalHeader>
+                <MDBModalBody>Deleted successfully</MDBModalBody>
+              </MDBModalContent>
+            </MDBModalDialog>
+          </MDBModal>
+        )}
+        {residences.map((residence, index) => {
           return (
-            <ResidenceCard
-              key={residence._id}
-              residence={residence}
-              deleteResidence={deleteResidence}
-            ></ResidenceCard>
+              <ResidenceCard
+                key={residence._id}
+                residence={residence}
+                deleteResidence={deleteResidence}
+                handleDeleteAlert={handleDeleteAlert}
+                fetchData={fetchData}
+              ></ResidenceCard>
           );
         })}
       </div>
